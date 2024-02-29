1829ecd5553578c86ae46bc2933b03b96cfc2a5b
Markus Weiss <maweiss@c4b4c2.42berlin.de>
2024-02-29 13:02:17 +0100

    First debugging and Valgrind

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 13:49:01 by maweiss           #+#    #+#             */
-/*   Updated: 2024/02/29 11:08:29 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/02/29 13:01:26 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,9 @@
 void	ft_lstfree(t_list **lst)
 {
 	void	*del;
-	t_list	**tmp_l;
 
 	del = &free;
-	tmp_l = lst;
-	while (tmp_l->next != NULL)
-	{
-		ft_lstclear(lst, del);
-		tmp_l = tmp_l->next;
-	}
+	ft_lstclear(lst, del);
 }
 
 int	ft_sorted(t_list *lst_a)
@@ -35,12 +29,14 @@ int	ft_sorted(t_list *lst_a)
 	tmp_l = lst_a;
 	value = INT_MIN;
 	sorted = 1;
-	while (tmp_l->next != NULL && sorted == 1)
+	while (sorted == 1)
 	{
-		if (value <= tmp_l->content)
+		if (value <= *(int *)tmp_l->content)
 		{
 			sorted = 1;
-			value = tmp_l->content;
+			value = *(int *)tmp_l->content;
+			if (tmp_l->next == NULL)
+				break ;
 			tmp_l = tmp_l->next;
 		}
 		else
@@ -49,31 +45,46 @@ int	ft_sorted(t_list *lst_a)
 	return (sorted);
 }
 
-void	ft_fill_lst(int *stack_a, int size)
+t_list	*ft_fill_lst(int **stack_a, int *size)
 {
 	t_list	*lst_a;
 	int		i;
 
 	i = 0;
-	lst_a = ft_lstnew(&stack_a[i++]);
-	while (stack_a[i])
-		ft_lstadd_back(&lst_a, ft_lstnew(stack_a[i++]));
+	lst_a = ft_lstnew(stack_a[i]);
+	stack_a[i++] = NULL;
+	while (i < *size)
+	{
+		ft_lstadd_back(&lst_a, ft_lstnew(stack_a[i]));
+		stack_a[i++] = NULL;
+	}
 	if (ft_sorted(lst_a))
 	{
-		ft_lstfree(lst_a);
+		ft_lstfree(&lst_a);
+		free(stack_a);
 		exit(3);
 	}
+	else
+		return (lst_a);
 }
 
-void	ft_switch(int *stack_a, int size, char purpose)
+void	ft_solve(int **stack_a, int *size)
+{
+	t_list	*lst_a;
+
+	lst_a = ft_fill_lst(stack_a, size);
+}
+
+void	ft_switch(int **stack_a, char purpose, int *size)
 {
 	if (purpose == 's')
 		ft_solve(stack_a, size);
 	else if (purpose == 'c')
-		ft_check(stack_a, size);
+		exit(1);
+	//ft_check(stack_a);
 }
 
-static void	ft_free(char **tofree)
+static void	ft_free(void **tofree)
 {
 	int	i;
 
@@ -98,7 +109,10 @@ int	ft_validate_args(char *str, int *valid)
 	i = 0;
 	*valid = 1;
 	if (!str)
-		return (NULL);
+	{
+		*valid = 0;
+		return (0);
+	}
 	if (str[i] == '-')
 		i++;
 	while (str[i] != '\0')
@@ -117,17 +131,19 @@ int	ft_validate_args(char *str, int *valid)
 	return (0);
 }
 
-char	*ft_parse_several(int argc, char **argv)
+int	**ft_parse_several(int argc, char **argv)
 {
 	int			i;
-	int			*stack_a;
+	int			**stack_a;
 	int			valid;
 
-	stack_a = (int *) malloc(sizeof(int) * (argc - 1));
+	stack_a = malloc(sizeof(int *) * (argc));
 	i = 0;
 	while (i <= (argc - 1))
 	{
-		stack_a[i++] = ft_atol(ft_validate_args(argv[i + 1], &valid)); // [ ] check if syntax is right (++)
+		stack_a[i] = malloc(sizeof(int));
+		*stack_a[i] = ft_validate_args(argv[i + 1], &valid); // [ ] check if syntax is right (++)
+		i++;
 		if (valid != 1)
 		{
 			free(stack_a);
@@ -137,30 +153,32 @@ char	*ft_parse_several(int argc, char **argv)
 	return (stack_a);
 }
 
-char	*ft_parse_one(char *input, int *size)
+int	**ft_parse_one(char *input, int *size)
 {
 	char	**split;
-	int		*stack_a;
+	int		**stack_a;
 	int		i;
 	int		valid;
 
 	i = ft_countwords(input, ' ');
-	stack_a = (int *) malloc(sizeof(int) * i);
-	split = ft_split(*input, ' ');
+	stack_a = malloc(sizeof(int *) * i + 1);
+	split = ft_split(input, ' ');
 	if (!split)
 		return (NULL);
 	i = 0;
 	while (split[i])
 	{
-		stack_a[i] = ft_atol(ft_validate_args(split[i], &valid));
+		stack_a[i] = malloc(sizeof(int));
+		*stack_a[i] = ft_validate_args(split[i], &valid);
 		if (valid != 1)
 		{
-			ft_free(split);
-			free(stack_a);
+			ft_free((void **)split);
+			ft_free((void **)stack_a);
 			return (NULL);
 		}
 		free(split[i++]);
 	}
+	stack_a[i] = NULL;
 	free(split);
 	*size = i; // [ ] validate right size from i
 	return (stack_a);
@@ -168,10 +186,9 @@ char	*ft_parse_one(char *input, int *size)
 
 int	main(int argc, char **argv)
 {
-	int		*stack_a;
+	int		**stack_a;
 	int		size;
 	char	purpose;
-	int		success;
 
 	purpose = 's'; // [ ] s = solve; c = check;
 	if (argc < 2)
@@ -183,7 +200,7 @@ int	main(int argc, char **argv)
 		stack_a = ft_parse_one(argv[1], &size);
 	else
 	{
-		stack_a = ft_parse_several(argc, **argv);
+		stack_a = ft_parse_several(argc, argv);
 		size = argc - 1;
 	}
 	if (!stack_a)
@@ -191,6 +208,6 @@ int	main(int argc, char **argv)
 		write(2, "ERROR\n", 6);
 		exit(2);
 	}
-	ft_switch(stack_a, size, purpose);
+	ft_switch(stack_a, purpose, &size);
 	return (1);
 }
