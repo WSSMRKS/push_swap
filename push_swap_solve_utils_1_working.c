a0c91a860807e9f6744ec9ef7a46c468f183db2b
Markus Weiss <maweiss@c4a12c5.42berlin.de>
2024-04-02 13:58:25 +0200

    working file utils_1

--- push_swap_solve_utils_1_working.c
+++ push_swap_solve_utils_1_working.c
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 14:37:36 by maweiss           #+#    #+#             */
-/*   Updated: 2024/03/27 18:18:38 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/03/27 18:09:13 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,29 @@ void	ft_calc_index(t_list **lst_a, int size)
 	long int	min_o;
 	int			index;
 	t_list		*tmp_a;
-	t_list		*tmp_min;
 
 	index = 0;
 	min_o = (long)-INT_MAX -2;
 	while (index < size)
 	{
-		tmp_min = NULL;
 		min = INT_MAX;
 		tmp_a = *lst_a;
 		while (tmp_a)
 		{
-			if ((tmp_min == NULL && (long) tmp_a->cont->value > min_o) || (tmp_a->cont->value <= tmp_min->cont->value && (long) tmp_a->cont->value > min_o))
-				tmp_min = tmp_a;
+			if (tmp_a->cont->value <= min && (long) tmp_a->cont->value > min_o)
+				min = tmp_a->cont->value;
 			tmp_a = tmp_a->next;
 		}
-		tmp_min->cont->index = index++;
-		ft_assign_chunk(&tmp_min, size, ft_chunks(size));
-		min_o = tmp_min->cont->value;
+		tmp_a = *lst_a;
+		while (tmp_a)
+		{
+			if (tmp_a->cont->value == min)
+				break ;
+			tmp_a = tmp_a->next;
+		}
+		tmp_a->cont->index = index++;
+		ft_assign_chunk(&tmp_a, size, ft_chunks(size));
+		min_o = (long) min;
 	}
 }
 
